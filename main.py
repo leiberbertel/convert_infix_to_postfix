940d0be6a44eb667bc9cc3ccc83dc59f5e678d4a
Leiber Bertel <leiberbertel777@gmail.com>
2023-02-02 19:35:32 -0500

    add class Conversion: init

---
+++ main.py
@@ -0,0 +1,10 @@
+class Conversion:
+    # Constructor to initialize the class variables
+    def __init__(self, capacity):
+        self.top = -1
+        self.capacity = capacity
+        # This array is used a stack
+        self.array = []
+        # Precedence setting
+        self.output = []
+        self.precedence = {'+': 1, '-': 1, '*': 2, '/': 2, '^': 3}
